9c17f0ada557e39b40aad62409ec84bf04547b5b
SMISHRA211 <97532647+SMISHRA211@users.noreply.github.com>
2023-04-14 21:24:37 +0530

    Create Process.php

---
+++ Process.php
@@ -0,0 +1,28 @@
+<?php 
+
+    if(isset($_POST['btn-send']))
+    {
+       $UserName = $_POST['UName'];
+       $Email = $_POST['Email'];
+       $Subject = $_POST['Subject'];
+       $Msg = $_POST['msg'];
+
+       if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
+       {
+           header('location:index.php?error');
+       }
+       else
+       {
+           $to = "mailbox.shivammishra@gmail.com";
+
+           if(mail($to,$Subject,$Msg,$Email))
+           {
+               header("location:index.php?success");
+           }
+       }
+    }
+    else
+    {
+        header("location:index.php");
+    }
+?>
